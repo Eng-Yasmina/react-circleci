c75b82a5e350f70634cd45b6dbfe6b4c32c6033f
Eng-Yasmina <aliyasoo70@gmail.com>
2022-12-20 14:12:25 +0200

    new content

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello React from circleci
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          thanks for watching
         </a>
       </header>
     </div>

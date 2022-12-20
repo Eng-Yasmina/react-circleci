4765017ca79bdad8f5934f752533eb8a1654f977
Eng-Yasmina <aliyasoo70@gmail.com>
2022-12-20 16:12:27 +0200

    change msg to netlify

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello React from circleci
+          Hello React from Netlify
         </p>
         <a
           className="App-link"
